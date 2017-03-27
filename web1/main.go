fae5a723d273444f8d62d7e16e231802128fb851
Andy Edward <83andyedward@gmail.com>
2017-03-27 23:40:01 +0800

    hahaha

---
+++ web1/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"html/template"
+	"regexp"
+	"fmt"
+)
+
+type Page struct {
+	Title 	string
+	Body 	[]byte
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("tmpl/*"))
+}
+
+func (p *Page) save() error {
+	filename:= p.Title+".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+
+}
+
+func loadPage(title string) (*Page,error) {
+	filename := "data/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+        return nil, err
+    }
+	return &Page{Title: title, Body:body}, nil
+}
+//var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    err := tpl.ExecuteTemplate(w, tmpl+".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+
+	p, err := loadPage(title)
+	if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+    
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    err := p.save()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		fmt.Println(m[2])
+        if m == nil {
+            http.NotFound(w, r)
+            return
+        }
+        fn(w, r, m[2])
+	}
+}
+
+func main() {
+    http.HandleFunc("/view/", makeHandler(viewHandler))
+    http.HandleFunc("/edit/", makeHandler(editHandler))
+    http.HandleFunc("/save/", makeHandler(saveHandler))
+    http.ListenAndServe(":8081", nil)
+}
